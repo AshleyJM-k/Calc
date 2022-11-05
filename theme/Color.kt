fc1ad36b98c7de9620d51e5fc87f03f900e72b72
Ashley McGraw-Korthals <101578859+AshleyJM-k@users.noreply.github.com>
2022-11-05 12:23:38 -0400

    Add files via upload

---
+++ theme/Color.kt
@@ -0,0 +1,9 @@
+package com.cis.calc2.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF26076B)
+val Teal200 = Color(0xFF03DAC5)
+val Gray800 = Color(0xE2353535)
